52b0a501798ed953c3fbaf990d466f49d678e465
Marvin Ernst <digital@cognitio.de>
2023-11-22 08:51:27 +0100

    cahnged config

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,20 @@ function defineNextConfig(config) {
 }
 
 export default defineNextConfig({
+  images: {
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  transpilePackages: ["next-image-export-optimizer"],
+  env: {
+    nextImageExportOptimizer_imageFolderPath: "public",
+    nextImageExportOptimizer_exportFolderPath: "out",
+    nextImageExportOptimizer_quality: "75",
+    nextImageExportOptimizer_storePicturesInWEBP: "true",
+    nextImageExportOptimizer_exportFolderName: "imagesp",
+    nextImageExportOptimizer_generateAndUseBlurImages: "true",
+  },
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
